0dc968393c8b3a65f163c52714ca6af6c3166af9
Belen Medina Roldan <bmedinaroldan@gmail.com>
2023-01-23 21:10:40 -0300

    Clase 5

--- src/components/TaskManager/Form/index.jsx
+++ src/components/TaskManager/Form/index.jsx
@@ -1,25 +1,27 @@
 import { Button, Form, } from "react-bootstrap";
 
-export default function FormTask({onChange, inputsValues, onSubmit, refForm}) {
-  return (
-    <Form onSubmit={onSubmit} ref={refForm}>
-        <Form.Group className="mb-3" >
-            <Form.Label>Titulo</Form.Label>
-            <Form.Control type="text" placeholder="Ingresar un titulo" value={inputsValues.title} onChange={onChange} name="title"/>            
-        </Form.Group>
+export default function FormTask({onChange, inputsValues, onSubmit, refForm, action}) {
+	return (
+		<Form onSubmit={onSubmit} ref={refForm}>
+			<Form.Group className="mb-3" >
+				<Form.Label>Titulo</Form.Label>
+				<Form.Control type="text" placeholder="Ingresar un titulo" value={inputsValues.title} onChange={onChange} name="title"/>            
+			</Form.Group>
 
-        <Form.Group className="mb-3" >
-            <Form.Label>Imagen</Form.Label>
-            <Form.Control type="text" placeholder="Ingresar un url" value={inputsValues.img} onChange={onChange} name="img"/>
-        </Form.Group>
+			<Form.Group className="mb-3" >
+				<Form.Label>Imagen</Form.Label>
+				<Form.Control type="text" placeholder="Ingresar un url" value={inputsValues.img} onChange={onChange} name="img"/>
+			</Form.Group>
 
-        <Form.Group className="mb-3" >
-            <Form.Label>Descripcion</Form.Label>
-            <Form.Control as={'textarea'} type="text" placeholder="Ingresar una descripcion" value={inputsValues.description} onChange={onChange} name="description"/>
-        </Form.Group>
-        
-        <Button variant="primary" type="submit" className="mx-2">Agregar</Button>
-        <Button variant="danger" type="reset">Reiniciar</Button>           
-    </Form>    
-  )
+			<Form.Group className="mb-3" >
+				<Form.Label>Descripcion</Form.Label>
+				<Form.Control as={'textarea'} type="text" placeholder="Ingresar una descripcion" value={inputsValues.description} onChange={onChange} name="description"/>
+			</Form.Group>
+			
+			<Button variant="primary" type="submit" className="mx-2">
+				{action === "CREATE" ? "Crear" : "Actualizar"}
+			</Button>
+			<Button variant="danger" type="reset">Reiniciar</Button>           
+		</Form>    
+	)
 }
--- src/components/TaskManager/index.jsx
+++ src/components/TaskManager/index.jsx
@@ -1,12 +1,50 @@
 import React from 'react';
-import { useRef } from "react";
+import { useReducer, useRef, useState } from 'react';
 import { Col, Container, Row } from "react-bootstrap";
 import { useForm } from "../../hooks/useForm";
+import { CardItem } from './CardItem';
 import FormTask from "./Form";
+
+const generateId = () => Math.random().toString(36).substring(2, 18);
+
+const taskReducer = (state, action) => { //{type, payload}
+    switch (action.type) {
+        case 'ADD':            
+            const newTask = {
+               ...action.payload,
+               id: generateId(),
+               active:false,
+               completed: false 
+            }
+            console.log(action.payload);
+            return [...state, newTask];
+
+        case 'UPDATE':
+            const taskToUpdate = action.payload;
+            const tasksUpdated = state.map((task) => {
+                if (task.id === taskToUpdate.id) {
+                    return {
+                        ...task,
+                        ...taskToUpdate
+                    }
+                }
+                return task
+            })
+            return tasksUpdated;
+    
+        default:
+            break;
+    }
+}
 
 export const TaskManager = () => {
     const refForm = useRef(null);
     const [inputsValues, setInputsValues, handleChangeInputValues, reset] = useForm({}, refForm);
+    //const [tasks, setTasks] = useState([{},{}]);
+
+    const [action, setAction] = useState("CREATE");
+
+    const [tasks, dispatch] = useReducer(taskReducer, []) //dispatch({type,payload})
 
 
     /* const [inputsValues, setInputsValues] = useState({});
@@ -21,7 +59,25 @@ export const TaskManager = () => {
 
     const handleSubmit = (e) =>{
         e.preventDefault();
+
+        if (action === "CREATE") {
+            dispatch({type: "ADD", payload: inputsValues});            
+        }
+
+        if (action === "UPDATE") {
+            dispatch({type: "UPDATE", payload: inputsValues});            
+        }
+
+
+        //setTasks([...tasks, inputsValues]);
         reset();
+    }
+
+    const handleUpdate = (id) => {
+        console.log("quiero actualizar item " + id);
+        const taskFound = tasks.find(task => task.id === id)
+        setInputsValues(taskFound)
+        setAction("UPDATE");
     }
 
 
@@ -29,9 +85,14 @@ export const TaskManager = () => {
         <Container className="mt-5">
             <Row>
                 <Col sm={12} lg={3}>
-                    <FormTask onChange={handleChangeInputValues} inputsValues={inputsValues} onSubmit={handleSubmit} refForm={{refForm}} />                     
+                    <FormTask onChange={handleChangeInputValues} inputsValues={inputsValues} onSubmit={handleSubmit} refForm={{refForm}} action={action} />                     
                 </Col>
-                <Col sm={12} lg={9}>            
+                <Col sm={12} lg={9}>
+                    {
+                        tasks.map((taskMap) => {
+                            return <CardItem key={taskMap.id} task={taskMap} onUpdate={handleUpdate} />
+                        })
+                    }
                 </Col>
             </Row>
         </Container>
